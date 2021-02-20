74b8a7d0457d7071769a4f931224cd75ebcc6715
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2021-02-20 01:24:32 +0100

    migrate the database to use identity only

---
+++ Server/Model/Migrations/V01/AddFisrtAndLastNameToUser/20210219193755_AddFirstAndLastNameToUser.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Order.Server.Model.Migrations.V01.AddFisrtAndLastNameToUser
+{
+    public partial class AddFirstAndLastNameToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                schema: "order_schema",
+                table: "user",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                schema: "order_schema",
+                table: "user",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                schema: "order_schema",
+                table: "user");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                schema: "order_schema",
+                table: "user");
+        }
+    }
+}
