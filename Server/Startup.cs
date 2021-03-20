3c1efd8601237c5ea067fa879a9faeda3050e2ce
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2021-03-20 22:03:44 +0100

    Redirection was not the problem

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -180,6 +180,13 @@ namespace Order.Server
                 app.UseMigrationsEndPoint();
                 app.UseWebAssemblyDebugging();
             }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
 
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
