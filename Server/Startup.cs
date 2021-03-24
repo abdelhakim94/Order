45b90dda7d3c7777a78de0ee87830a8f525134b2
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2021-03-25 00:42:41 +0100

    fix https redirection infinite loop

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -47,6 +47,23 @@ namespace Order.Server
             services.AddSingleton(emailBoxConfig);
             #endregion
 
+            if (string.Equals(
+                Environment.GetEnvironmentVariable("ASPNETCORE_FORWARDEDHEADERS_ENABLED"),
+                "true",
+                StringComparison.OrdinalIgnoreCase))
+            {
+                services.Configure<ForwardedHeadersOptions>(options =>
+                {
+                    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor |
+                        ForwardedHeaders.XForwardedProto;
+                    // Only loopback proxies are allowed by default.
+                    // Clear that restriction because forwarders are enabled by explicit 
+                    // configuration.
+                    options.KnownNetworks.Clear();
+                    options.KnownProxies.Clear();
+                });
+            }
+
             services.AddDbContextPool<IOrderContext, OrderContext>(builder =>
                 builder.UseNpgsql(Configuration.GetConnectionString("dev_db_order")
             ));
