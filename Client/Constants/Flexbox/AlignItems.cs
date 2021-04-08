b26908048966af4619a4ba960d5bbb4f4a1fc223
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2021-04-08 15:45:08 +0200

    Create layout components (#40)
    * create row component
    
    * create column component

---
+++ Client/Constants/Flexbox/AlignItems.cs
@@ -0,0 +1,11 @@
+namespace Order.Client.Constants
+{
+    public class AlignItems
+    {
+        public const string FLEX_START = "flex-start";
+        public const string FLEX_END = "flex-end";
+        public const string CENTER = "center";
+        public const string BASELINE = "baseline";
+        public const string STRETCH = "stretch";
+    }
+}
---
+++ Client/Constants/Flexbox/JustifyContent.cs
@@ -0,0 +1,11 @@
+namespace Order.Client.Constants
+{
+    public class JustifyContent
+    {
+        public const string FLEX_START = "flex-start";
+        public const string FLEX_END = "flex-end";
+        public const string CENTER = "center";
+        public const string SPACE_BETWEEN = "space-between";
+        public const string SPACE_AROUND = "space-around";
+    }
+}
