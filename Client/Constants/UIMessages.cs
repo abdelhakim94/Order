643e5b48b97e1124927ec8a0ba79f3af1157172b
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2021-06-05 18:53:14 +0200

    Add dish details page

--- Client/Constants/UIMessages.cs
+++ Client/Constants/UIMessages.cs
@@ -92,7 +92,7 @@ namespace Order.Client.Constants
         public static string Remaining { get => "éléments de plus"; }
 
         public static string Options { get => "Options:"; }
-        public static string Extras { get => "Supplément:"; }
+        public static string Extras { get => "Suppléments:"; }
 
         public static string AddToCart { get => "Ajouter au panier"; }
     }
